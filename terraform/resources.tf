0f5097ce5f3db3b35a77aa976e64da6c0fda3029
Jang Eui Jin <joomanba@gmail.com>
2020-10-12 12:12:58 +0900

    Modify Code According to Terraform Version

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
+  # access_key = var.access_key
+  # secret_key = var.secret_key
+  region = var.region
 }
 
 data "aws_ami" "ec2-ami" {
@@ -9,7 +9,7 @@ data "aws_ami" "ec2-ami" {
     name   = "state"
     values = ["available"]
   }
-  owners  = ["self"]
+  owners = ["self"]
   filter {
     name   = "tag:Name"
     values = ["Packer-Ansible"]
@@ -18,32 +18,33 @@ data "aws_ami" "ec2-ami" {
   most_recent = true
 }
 
+
 data "terraform_remote_state" "network" {
   backend = "local"
 
-  config {
+  config = {
     path = "../networkTerraform/terraform.tfstate"
   }
 }
 
 module "securityGroupModule" {
-    source			= "./modules/securityGroup"
- 	access_key		= "${var.access_key}"
-	secret_key		= "${var.secret_key}"
-	region			= "${var.region}"
-	vpc_id			= "${data.terraform_remote_state.network.vpc_id}"
-	environment_tag = "${var.environment_tag}"
+  source = "./modules/securityGroup"
+  # access_key      = "var.access_key"
+  # secret_key      = "var.secret_key"
+  region          = var.region
+  vpc_id          = data.terraform_remote_state.network.outputs.vpc_id
+  environment_tag = var.environment_tag
 }
 
 module "instanceModule" {
-	source 				= "./modules/instance"
-	access_key 			= "${var.access_key}"
- 	secret_key 			= "${var.secret_key}"
- 	region     			= "${var.region}"
- 	instance_ami		= "${data.aws_ami.ec2-ami.id}"
- 	vpc_id 				= "${data.terraform_remote_state.network.vpc_id}"
-	subnet_public_id	= "${data.terraform_remote_state.network.public_subnets[0]}"
-	key_pair_name		= "${data.terraform_remote_state.network.ec2keyName}"
-	security_group_ids 	= ["${module.securityGroupModule.sg_22}", "${module.securityGroupModule.sg_80}"]
-	environment_tag 	= "${var.environment_tag}"
+  source = "./modules/instance"
+  # access_key         = var.access_key
+  # secret_key         = var.secret_key
+  region             = var.region
+  instance_ami       = data.aws_ami.ec2-ami.id
+  vpc_id             = data.terraform_remote_state.network.outputs.vpc_id
+  subnet_public_id   = data.terraform_remote_state.network.outputs.public_subnets[0]
+  key_pair_name      = data.terraform_remote_state.network.outputs.ec2keyName
+  security_group_ids = ["${module.securityGroupModule.sg_22}", "${module.securityGroupModule.sg_80}"]
+  environment_tag    = var.environment_tag
 }
